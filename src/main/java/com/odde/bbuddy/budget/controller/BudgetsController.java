5d8f0dcda9e3a47e3a3670a644f6b3052cd6c251
philip.wang <philip.wang@perficientgdc.com.cn>
2017-05-22 19:02:31 +0800

    Budget invalid message: merge

--- src/main/java/com/odde/bbuddy/budget/controller/BudgetsController.java
+++ src/main/java/com/odde/bbuddy/budget/controller/BudgetsController.java
@@ -2,13 +2,18 @@ package com.odde.bbuddy.budget.controller;
 
 import com.odde.bbuddy.budget.Budget;
 import com.odde.bbuddy.budget.domain.Budgets;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.logging.SimpleFormatter;
+
+import static com.odde.bbuddy.common.Formats.DAY;
 
 @Controller
 public class BudgetsController {
@@ -33,11 +38,14 @@ public class BudgetsController {
         return modelAndView;
     }
 
+    @GetMapping("/budgets/sum")
+    public String sum() {return  "/budgets/sum";}
+
     @PostMapping("/budgets/sum")
-    public ModelAndView getAmountSum(@ModelAttribute String startTime,String endTime){
-
-
-       return null;
+    public ModelAndView getSum(String startDate, String endDate) throws ParseException {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/budgets/sum");
+        modelAndView.addObject("sum",budgets.getSum(startDate,endDate));
+        return modelAndView;
     }
-
 }
