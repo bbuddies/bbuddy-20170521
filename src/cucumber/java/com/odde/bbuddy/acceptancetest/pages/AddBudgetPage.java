f17da3b0eadcefe3029c0d45633058c457377225
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-05-22 20:29:37 +0800

    fix test issues

--- src/cucumber/java/com/odde/bbuddy/acceptancetest/pages/AddBudgetPage.java
+++ src/cucumber/java/com/odde/bbuddy/acceptancetest/pages/AddBudgetPage.java
@@ -2,7 +2,11 @@ package com.odde.bbuddy.acceptancetest.pages;
 
 import com.odde.bbuddy.acceptancetest.driver.UiDriver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component
+@Scope("cucumber-glue")
 public class AddBudgetPage {
 
     @Autowired
--- src/test/java/com/odde/bbuddy/budget/domain/BudgetsTest.java
+++ src/test/java/com/odde/bbuddy/budget/domain/BudgetsTest.java
@@ -8,9 +8,7 @@ import org.mockito.ArgumentCaptor;
 import static com.odde.bbuddy.common.Formats.parseMonth;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BudgetsTest {
     BudgetRepo mockBudgetRepo = mock(BudgetRepo.class);
